d12c84add6c2d9de2d1f7420a48b4b8c30e92c12
Vlad Zotov <sotowwlad@gmail.com>
2022-03-28 13:53:59 +0300

    change script initTableImages.sql - rename char() to text

--- src/main/resources/db/initTableImages.sql
+++ src/main/resources/db/initTableImages.sql
@@ -1,9 +1,9 @@
 create table images
 (
     id                int auto_increment,
-    name              varchar(100) not null,
-    original_filename varchar(100) not null,
-    content_type      varchar(100) not null,
+    name              text not null,
+    original_filename text not null,
+    content_type      text not null,
     size              int          not null,
     bytes             longblob     not null,
     constraint images_pk
